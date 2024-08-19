39ad87fd359a36106382f236d38fe03e2ac5153b
hasethakuru <hassanthakuru@proton.me>
2024-08-19 22:08:24 +0530

    Commit
    - Add dynamic image swiper
    - Fixed issue #32

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,13 @@
 // DOM ELEMENTS
 const newsBannerSection = document.querySelector('.news-banner');
 const newsBannerText = document.querySelector('.breaking-news-headline');
-const hasAutoNumber = Array.from(document.querySelectorAll('.has-auto-number'));
 var loader = document.getElementById("preloader");
 const circleTopButton = document.getElementById("top");
 
 newsBannerText.innerText = ''
 
 window.addEventListener("load", function () {
-loader.style.display = "none";
+    loader.style.display = "none";
 })
 
 circleTopButton.addEventListener('click', ()=> {
@@ -30,39 +29,9 @@ $('.a-nav-toggle, .menu-main a').on('click', function(){
     }
 });
 
-var swiper = new Swiper(".mySwiper", {
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    loop: true,
-});
 
 if (newsBannerText.innerText == false) {
     newsBannerSection.style.display = 'none';
 } else {
     newsBannerSection.style.display = 'block';
 }
-
-// Automatic Numbering
-let orderNumbers = [
-    'first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth',
-    'eleventh', 'twelfth', 'thirteenth', 'fourteenth', 'fifteenth', 'sixteenth', 'seventeenth', 'eighteenth', 'nineteenth', 'twentieth',
-    'twenty-first', 'twenty-second', 'twenty-third', 'twenty-fourth', 'twenty-fifth', 'twenty-sixth', 'twenty-seventh', 'twenty-eighth', 'twenty-ninth', 'thirtieth',
-    'thirty-first', 'thirty-second', 'thirty-third', 'thirty-fourth', 'thirty-fifth', 'thirty-sixth', 'thirty-seventh', 'thirty-eighth', 'thirty-ninth', 'fortieth',
-    'forty-first', 'forty-second', 'forty-third', 'forty-fourth', 'forty-fifth', 'forty-sixth', 'forty-seventh', 'forty-eighth', 'forty-ninth', 'fiftieth',
-    'fifty-first', 'fifty-second', 'fifty-third', 'fifty-fourth', 'fifty-fifth', 'fifty-sixth', 'fifty-seventh', 'fifty-eighth', 'fifty-ninth', 'sixtieth',
-    'sixty-first', 'sixty-second', 'sixty-third', 'sixty-fourth', 'sixty-fifth', 'sixty-sixth', 'sixty-seventh', 'sixty-eighth', 'sixty-ninth', 'seventieth',
-    'seventy-first', 'seventy-second', 'seventy-third', 'seventy-fourth', 'seventy-fifth', 'seventy-sixth', 'seventy-seventh', 'seventy-eighth', 'seventy-ninth', 'eightieth',
-    'eighty-first', 'eighty-second', 'eighty-third', 'eighty-fourth', 'eighty-fifth', 'eighty-sixth', 'eighty-seventh', 'eighty-eighth', 'eighty-ninth', 'ninetieth',
-    'ninety-first', 'ninety-second', 'ninety-third', 'ninety-fourth', 'ninety-fifth', 'ninety-sixth', 'ninety-seventh', 'ninety-eighth', 'ninety-ninth', 'hundredth'
-];
-
-hasAutoNumber.forEach(n => {
-    const orderNumberElements = Array.from(n.querySelectorAll('.order-number'));
-    for (let r = 0; r < orderNumberElements.length; r++) {
-        orderNumberElements[r].innerText = orderNumbers[r] + ' review';
-        orderNumberElements[r].style.textTransform = 'uppercase';
-    }
-});
-
